0dfb088f94f03b10ad925d46fff69342214e44af
pravesh18 <54497006+pravesh18@users.noreply.github.com>
2020-02-07 16:49:47 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Pravesh Vyas, Really?`,
-        short_name: `Cara`,
+        short_name: `PRV`,
         description: `Shifting Paradigms in web Experiences`,
         start_url: `/`,
         background_color: `#141821`,
