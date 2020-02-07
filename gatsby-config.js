0570773067f615933d9e298ccd03bb0410193f83
pravesh18 <54497006+pravesh18@users.noreply.github.com>
2020-02-07 17:05:51 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-     Shifting Paradigms in web Experiences
+     "Shifting Paradigms in web Experiences"
     siteTitleAlt: `Pravesh Vyas`,
   },
   plugins: [
